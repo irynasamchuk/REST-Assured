0339822adf77d237f541dd079d490be58766a9d2
Iryna Samchuk <irynasamchuk19@gmail.com>
2019-12-13 14:37:30 +0200

    Add Listeners for extent reports

--- src/test/java/base/TestBase.java
+++ src/test/java/base/TestBase.java
@@ -6,11 +6,6 @@ import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 import org.apache.log4j.PropertyConfigurator;
 import org.testng.annotations.BeforeClass;
-import org.testng.annotations.Test;
-
-import java.util.ArrayList;
-import java.util.List;
-
 
 public class TestBase {
     public static RequestSpecification httpRequest;
